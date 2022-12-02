c732ffc9f3ea17d6b4ec8e06332a746ce146ff51
Hiebeler <emanuel.hiebeler@gmail.com>
2022-12-02 12:58:44 +0100

    added queue

---
+++ MyQueue/MyQueue.cs
@@ -0,0 +1,33 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQueue
+{
+    public class MyQueue
+    {
+        private SingleLinkedList.SingleLinkedList internalList = new SingleLinkedList.SingleLinkedList();
+
+        public object Enqueue(object argValue)
+        {
+            internalList.InsertFront(argValue);
+            return argValue;
+        }
+
+        public Node Dequeue()
+        {
+            var retval = internalList.First();
+            internalList.DeleteFirst();
+            return retval;
+        }
+
+        public override string ToString()
+        {
+            return internalList.ToString();
+        }
+
+    }
+}
