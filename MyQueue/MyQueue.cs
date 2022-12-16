3f7178c710487439fc97ba57f1d477fcb4451f9a
Hiebeler <emanuel.hiebeler@gmail.com>
2022-12-16 09:37:56 +0100

    added observer pattern

--- MyQueue/MyQueue.cs
+++ MyQueue/MyQueue.cs
@@ -7,13 +7,14 @@ using System.Threading.Tasks;
 
 namespace MyQueue
 {
-    public class MyQueue
+    public class MyQueue : ISubject
     {
         private SingleLinkedList.SingleLinkedList internalList = new SingleLinkedList.SingleLinkedList();
-
+        private List<IObserver> _observers = new List<IObserver>();
         public object Enqueue(object argValue)
         {
             internalList.InsertLast(argValue);
+            Notify();
             return argValue;
         }
 
@@ -21,8 +22,31 @@ namespace MyQueue
         {
             var retval = internalList.First();
             internalList.DeleteFirst();
+            Notify();
             return retval;
         }
+
+        public void Attach(IObserver observer)
+        {
+            Console.WriteLine("Subject: Attached an observer.");
+            this._observers.Add(observer);
+        }
+        public void Detach(IObserver observer)
+        {
+            this._observers.Remove(observer);
+            Console.WriteLine("Subject: Detached an observer.");
+        }
+
+        public void Notify()
+        {
+            Console.WriteLine("Subject: Notifying observers...");
+
+            foreach (var observer in _observers)
+            {
+                observer.Update(this);
+            }
+        }
+
 
         public override string ToString()
         {
--- SortAlgorithmTest/SelectionSortTest.cs
+++ SortAlgorithmTest/SelectionSortTest.cs
@@ -28,11 +28,15 @@ namespace SortAlgorithmTest
         {
             var linkedList = new SingleLinkedList.SingleLinkedList();
             linkedList.InsertFront(3);
+            linkedList.InsertFront(6);
+            linkedList.InsertFront(4);
             linkedList.InsertFront(1);
+            linkedList.InsertFront(7);
+            linkedList.InsertFront(5);
             linkedList.InsertLast(2);
             linkedList.SetSortStrategy(new SelectionSort());
             linkedList.SortDescending();
-            Assert.AreEqual(linkedList.ToString(), "3\n2\n1\n");
+            Assert.AreEqual(linkedList.ToString(), "7\n6\n5\n4\n3\n2\n1\n");
         }
     }
 }
